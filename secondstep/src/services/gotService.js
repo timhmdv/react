4e8ef5b7edabf4c2ac5d2bdebdc86e8522777926
timhmdv <dakota4126@gmail.com>
2023-01-17 14:49:38 +0400

    Added a notification message when some field was not received from the server

--- secondstep/src/services/gotService.js
+++ secondstep/src/services/gotService.js
@@ -42,31 +42,33 @@ export default class GotService {
 
   _transformCharacter(character) {
     return {
-      name: character.name,
-      gender: character.gender,
-      born: character.born,
-      died: character.died,
-      culture: character.culture,
+      name: character.name ? character.name : 'No name info',
+      gender: character.gender ? character.gender : 'No gender info',
+      born: character.born ? character.born : 'No born info',
+      died: character.died ? character.died : 'No death info',
+      culture: character.culture ? character.culture : 'No culture info',
     };
   }
 
   _transformHouse(house) {
     return {
-      name: house.name,
-      region: house.region,
-      words: house.words,
-      titles: house.titles,
-      overlord: overlord.overlord,
-      ancestalWeapons: house.ancestalWeapons,
+      name: house.name ? house.name : 'No name info',
+      region: house.region ? house.region : 'No region info',
+      words: house.words ? house.words : 'No words info',
+      titles: house.titles ? house.titles : 'No titles info',
+      overlord: house.overlord ? house.overlord : 'No overlord info',
+      ancestalWeapons: house.ancestalWeapons ?
+      house.ancestalWeapons : 'No ancestal weapons info',
     };
   }
 
   _transformHouse(book) {
     return {
-      name: book.name,
-      numberOfPages: book.numberOfPages,
-      publisher: book.publisher,
-      released: book.released,
+      name: book.name ? book.name : 'No name info',
+      numberOfPages: book.numberOfPages ?
+      book.numberOfPages : 'No number of pages info',
+      publisher: book.publisher ? book.publisher : 'No publisher info',
+      released: book.released ? book.released : 'No release info',
     };
   }
 };
