8473131ceaf6e7236953c8eb34cf40a11c64f3cc
timhmdv <dakota4126@gmail.com>
2023-01-17 14:40:52 +0400

    added error mesaage component

---
+++ secondstep/src/components/errorMessage/errorMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './errorMessage.css';
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <img></img>
+      <span className='error'>Something went wrong!</span>
+    </>
+  );
+};
+
+export default ErrorMessage;
