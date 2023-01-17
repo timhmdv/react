76f34f20aa0556c1edb809ec1c4a914a27918103
timhmdv <dakota4126@gmail.com>
2023-01-17 14:37:31 +0400

    added fucntionality to trigger random char block via button

--- secondstep/src/components/app/app.js
+++ secondstep/src/components/app/app.js
@@ -1,34 +1,55 @@
-import React from 'react';
+/* eslint-disable require-jsdoc */
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import './app.css';
 
 
-const App = () => {
-  return (
-    <>
-      <Container>
-        <Header />
-      </Container>
-      <Container>
-        <Row>
-          <Col lg={{size: 5, offset: 0}}>
-            <RandomChar/>
-          </Col>
-        </Row>
-        <Row>
-          <Col md='6'>
-            <ItemList />
-          </Col>
-          <Col md='6'>
-            <CharDetails />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    showRandomCharacter: true,
+  };
+
+  toggleRandomCharacter = () => {
+    this.setState({
+      showRandomCharacter: !this.state.showRandomCharacter,
+    });
+  };
+
+  render() {
+    const {showRandomCharacter} = this.state;
+
+    return (
+      <>
+        <Container>
+          <Header />
+        </Container>
+        <Container>
+          <Row>
+            <Col lg={{size: 5, offset: 0}}>
+              {showRandomCharacter ? <RandomChar/> : null}
+              <button className='button' onClick={this.toggleRandomCharacter}>
+                Toggle random character
+              </button>
+            </Col>
+          </Row>
+          <Row>
+            <Col md='6'>
+              <ItemList />
+            </Col>
+            <Col md='6'>
+              <CharDetails />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 };
-
-export default App;
