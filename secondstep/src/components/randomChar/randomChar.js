5d4e489c38dbef15cc9d12c4c36f25de77b34701
timhmdv <dakota4126@gmail.com>
2023-01-17 14:50:50 +0400

    added functionality for notifying the user about module loading or error

--- secondstep/src/components/randomChar/randomChar.js
+++ secondstep/src/components/randomChar/randomChar.js
@@ -1,7 +1,10 @@
 /* eslint-disable require-jsdoc */
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './randomChar.css';
 import GotService from '../../services/gotService';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 export default class RandomChar extends Component {
   constructor(props) {
@@ -14,10 +17,22 @@ export default class RandomChar extends Component {
 
   state = {
     character: {},
+    loading: true,
   };
 
   onCharLoaded = (character) => {
-    this.setState({character});
+    this.setState({
+      character,
+      loading: false,
+      error: false,
+    });
+  };
+
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
   };
 
   updateCharacter() {
@@ -25,33 +40,61 @@ export default class RandomChar extends Component {
     this.gotService.getCharacter(id)
         .then((character) => {
           this.onCharLoaded(character);
-        });
+        })
+        .catch(this.onError);
   }
 
   render() {
-    const {character: {name, gender, born, died, culture}} = this.state;
+    const {character, loading, error} = this.state;
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View character={character}/> : null;
+
     return (
       <div className="random-block rounded">
-        <h4>Random Character: {name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Gender </span>
-            <span>{gender}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Born </span>
-            <span>{born}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Died </span>
-            <span>{died}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Culture </span>
-            <span>{culture}</span>
-          </li>
-        </ul>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const View = ({character}) => {
+  const {name, gender, born, died, culture} = character;
+
+  return (
+    <>
+      <h4>Random Character: {name}</h4>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Gender </span>
+          <span>{gender}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Born </span>
+          <span>{born}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Died </span>
+          <span>{died}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Culture </span>
+          <span>{culture}</span>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+View.propTypes = {
+  character: PropTypes.object,
+  name: PropTypes.string,
+  gender: PropTypes.string,
+  born: PropTypes.string,
+  died: PropTypes.string,
+  culture: PropTypes.string,
+};
+
