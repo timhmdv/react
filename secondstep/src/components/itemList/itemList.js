c5ae9a3591ee18168daceaec2a34efb1bff6d64b
timhmdv <dakota4126@gmail.com>
2023-01-18 18:21:38 +0400

    added functionaity that fetches and adds characters to list element

--- secondstep/src/components/itemList/itemList.js
+++ secondstep/src/components/itemList/itemList.js
@@ -1,21 +1,56 @@
+/* eslint-disable require-jsdoc */
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './itemList.css';
-// eslint-disable-next-line require-jsdoc
+import GotService from '../../services/gotService';
+import Spinner from '../spinner';
+
 export default class ItemList extends Component {
-  // eslint-disable-next-line require-jsdoc
+  GotService = new GotService();
+
+  state = {
+    charactersList: null,
+  };
+
+  componentDidMount() {
+    this.GotService.getAllCharacters()
+        .then((charactersList) => {
+          this.setState({
+            charactersList,
+          });
+        });
+  };
+
+  renderItems(arr) {
+    return arr.map((character, index) => {
+      return (
+        <li
+          key={index}
+          className="list-group-item">
+          {character.name}
+        </li>
+      );
+    });
+  };
+
   render() {
+    const {charactersList} = this.state;
+
+    if (!charactersList) {
+      return <Spinner/>;
+    }
+
+    console.log(this.renderItems(charactersList));
+    const characters = this.renderItems(charactersList);
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-                    John Snow
-        </li>
-        <li className="list-group-item">
-                    Brandon Stark
-        </li>
-        <li className="list-group-item">
-                    Geremy
-        </li>
+        {characters}
       </ul>
     );
   }
 }
+
+ItemList.propTypes = {
+  onCharacterSelected: PropTypes.func.isRequired,
+};
