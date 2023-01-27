fe1af7a90d91310d3c29f26122d7117b266070bc
timhmdv <dakota4126@gmail.com>
2023-01-27 18:34:17 +0400

    exports reworked

---
+++ secondstep/src/components/pages/index.js
@@ -0,0 +1,9 @@
+import CharactersPage from './charactersPage/charactersPage';
+import HousesPage from './housesPage/housesPage';
+import BooksPage from './booksPage/booksPage';
+
+export {
+  CharactersPage,
+  HousesPage,
+  BooksPage,
+};
